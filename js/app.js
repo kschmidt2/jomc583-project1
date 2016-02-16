0828746768d94ee587fa4ea3c177954ec9efa313
kschmidt2 <k.schmidt2@gmail.com>
2016-02-16 12:39:59 -0500

    Wednesday afternoon stopping point

--- js/app.js
+++ js/app.js
@@ -13,14 +13,67 @@ app.controller('SenateController', ['$http', function($http) {
       .error(function(msg) {
       });
 
-      // _this.isClosed = false;
-      // _this.isClosed {
-      //   if (_this.open === false) {
-      //     _this.isClosed = true;
-      //   } else {
-      //     _this.isClosed = false;
-      //   }
-      // };
+    //show and hide more info div
+    this.getInfo = function(seatName) {
+
+      for (i = 0; i < this.seats.length; i++) {
+
+        if (seatName == this.seats[i].name) {
+
+          var seatInfo = "";
+
+          console.log("this worked!");
+
+          if (this.seats[i].incumbent.party == "Republican") {
+
+            seatInfo += '<div class="repstrip"><h3>current senator</h3></div><div class="senatorname">' + this.seats[i].incumbent.name + '</div><div class="senatorparty">' + this.seats[i].incumbent.party + "</div>";
+
+          } else {
+            seatInfo += '<div class="demstrip"><h3>current senator</h3></div><div class="senatorname">' + this.seats[i].incumbent.name + '</div><div class="senatorparty">' + this.seats[i].incumbent.party + "</div>";
+          }
+
+
+
+          if (this.seats[i].open == "open") {
+
+            if (this.seats[i].featuredCandidate.party == "Republican") {
+
+              seatInfo += '<div class="repstrip"><h3>featured candidate</h3></div>';
+              seatInfo += '<img class="candidatephoto" src="' + this.seats[i].featuredCandidate.photo + '">';
+              seatInfo += '<div class="senatorname">' + this.seats[i].featuredCandidate.name + '</div><div class="featuredparty">' + this.seats[i].featuredCandidate.party + "</div>";
+              seatInfo += '<div class="moreinfo"><b style="text-transform:uppercase">About: </b>' + this.seats[i].featuredCandidate.bio + '<br /><b style="text-transform:uppercase">Platform points: </b>' + this.seats[i].featuredCandidate.platform + '</div>';
+
+            } else {
+
+              seatInfo += '<div class="demstrip"><h3>featured candidate</h3></div>';
+              seatInfo += '<img class="candidatephoto" src="' + this.seats[i].featuredCandidate.photo + '">';
+              seatInfo += '<div class="senatorname">' + this.seats[i].featuredCandidate.name + '</div><div class="featuredparty">' + this.seats[i].featuredCandidate.party + "</div>";
+              seatInfo += '<div class="moreinfo"><b style="text-transform:uppercase">About: </b>' + this.seats[i].featuredCandidate.bio + '<br /><b style="text-transform:uppercase">Platform points: </b>' + this.seats[i].featuredCandidate.platform + '</div>';
+
+            }
+
+            if (this.seats[i].otherCandidates !== "none") {
+
+              console.log("this also worked!");
+
+              seatInfo += '<div class="otherstrip"><h3>other candidates</h3></div>';
+
+              for (j = 0; j < this.seats[i].otherCandidates.length; j++) {
+                console.log("more candidates success");
+
+                seatInfo += '<div class="othercandidates"><b>' + this.seats[i].otherCandidates[j].name + '</b>, ' + this.seats[i].otherCandidates[j].party + '</div>';
+              }
+
+            }
+
+          }
+
+          document.getElementById("sidebar").innerHTML = seatInfo;
+
+        }
+      }
+
+    }
 }]);
 
 
